e2c8b6e4c499a92c5ce25352bec911bec07c618d
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2023-08-05 20:26:54 +0600

    Update my_model.dart

--- lib/data/models/my_model.dart
+++ lib/data/models/my_model.dart
@@ -16,3 +16,23 @@
 
 // This is the json we are getting
 
+class TodoModel {
+  TodoModel({
+    required this.id,
+    required this.title,
+    required this.isCompleted,
+  });
+
+  final String id;
+  final String title;
+  final bool isCompleted;
+
+  factory TodoModel.fromJson(Map<String, dynamic> json) {
+    return TodoModel(
+      id: json['id'],
+      title: json['title'],
+      isCompleted: json['completed'],
+    );
+  }
+}
+
