94caa56c6e297a08af221df99982413e5e28a68d
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2023-08-05 20:26:50 +0600

    Create my_model.dart

---
+++ lib/data/models/my_model.dart
@@ -0,0 +1,18 @@
+/*
+{
+  "data": {
+    "todos": {
+      "data": [
+        {
+          "id": "1",
+          "title": "delectus aut autem",
+          "completed": false
+        },
+      ]
+    }
+  }
+}
+*/
+
+// This is the json we are getting
+
