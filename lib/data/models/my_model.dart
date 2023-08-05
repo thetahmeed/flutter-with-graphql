dd1f2cb086d57cfbfbbef045b444f9731fd38336
Tahmeedul Islam <tahmeed.com.bd@gmail.com>
2023-08-05 20:26:57 +0600

    Update my_model.dart

--- lib/data/models/my_model.dart
+++ lib/data/models/my_model.dart
@@ -36,3 +36,16 @@ class TodoModel {
   }
 }
 
+class TodoList {
+  final List<TodoModel> todos;
+
+  TodoList({required this.todos});
+
+  factory TodoList.fromJson(Map<String, dynamic> json) {
+    return TodoList(
+      todos: (json['data'] as List<dynamic>)
+          .map((todoJson) => TodoModel.fromJson(todoJson))
+          .toList(),
+    );
+  }
+}
